dcd469d0d091befc41bfe905a8920dc5d566b9d8
Kobenko Mikhail <m.kobenko@tinkoff.ru>
2018-05-30 15:02:33 +0500

    +

---
+++ app/Kleisli.hs
@@ -0,0 +1,18 @@
+module Kleisli where
+
+import           Prelude hiding (id, (>>), (*>))
+
+class Category cat where
+    id :: cat a a
+    (>>) :: cat a b -> cat b c -> cat a c
+
+class Kleisli m where
+    idK :: a -> m a
+    (*>) :: (a -> m b) -> (b -> m c) -> (a -> m c)
+
+(+>) :: Kleisli m => (a -> m b) -> (b -> c) -> (a -> m c)
+f +> g = f *> (g >> idK)
+
+instance Category (->) where
+    id = \x -> x
+    f >> g = \x -> g (f x)
